01f4826056f1423f45397e962493f0629dde2b0c
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-03-04 17:37:03 -0500

    Added components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { AddTransaction } from "./Components/AddTransaction";
+import { Balance } from "./Components/Balance";
+import { Header } from "./Components/Header";
+import { IncomeExpenses } from "./Components/IncomeExpenses";
+import { TansactionList } from "./Components/TansactionList";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TansactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
 }
 
 export default App;
